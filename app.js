ff347894e5e7d0e2de063d920540ad7d43ca814c
Sinisa Koscec <snsa_kscc@asdf.local>
2021-12-06 00:37:34 +0100

    4 new products early alpha

--- app.js
+++ app.js
@@ -16,21 +16,13 @@ if (sound) {
 let accordion = document.querySelector(".accordion");
 
 if (accordion) {
-  let detailsHeight = document.querySelector(
-    ".accordion-details__list"
-  ).offsetHeight;
+  let detailsHeight = document.querySelector(".accordion-details__list").offsetHeight;
   let sizeHeight = document.querySelector(".accordion-size__list").offsetHeight;
   let shippingHeight = document.querySelector(".accordion__copy").offsetHeight;
   let style = document.createElement("style");
-  style.innerHTML = `input[name="panel"]:checked ~ .accordion__content--details {height: ${
-    detailsHeight + 30
-  }px;}
-    input[name="panel"]:checked ~ .accordion__content--size {height: ${
-      sizeHeight + 30
-    }px;}
-    input[name="panel"]:checked ~ .accordion__content--shipping {height: ${
-      shippingHeight + 30
-    }px;}`;
+  style.innerHTML = `input[name="panel"]:checked ~ .accordion__content--details {height: ${detailsHeight + 30}px;}
+    input[name="panel"]:checked ~ .accordion__content--size {height: ${sizeHeight + 30}px;}
+    input[name="panel"]:checked ~ .accordion__content--shipping {height: ${shippingHeight + 30}px;}`;
   document.head.appendChild(style);
 }
 
@@ -44,19 +36,11 @@ setTimeout(() => {
       smooth: true,
     });
     locoScroll.on("scroll", () => {
-      const currentScroll = Math.round(
-        locoScroll.scroll.instance.scroll.y / 100
-      );
+      const currentScroll = Math.round(locoScroll.scroll.instance.scroll.y / 100);
 
-      if (
-        currentScroll > lastScroll &&
-        !body.classList.contains("scroll-down")
-      ) {
+      if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
         body.classList.add("scroll-down");
-      } else if (
-        currentScroll < lastScroll &&
-        body.classList.contains("scroll-down")
-      ) {
+      } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
         body.classList.remove("scroll-down");
       }
       lastScroll = currentScroll;
@@ -65,15 +49,9 @@ setTimeout(() => {
     window.addEventListener("scroll", () => {
       const currentScroll = Math.round(window.pageYOffset / 50);
 
-      if (
-        currentScroll > lastScroll &&
-        !body.classList.contains("scroll-down")
-      ) {
+      if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
         body.classList.add("scroll-down");
-      } else if (
-        currentScroll < lastScroll &&
-        body.classList.contains("scroll-down")
-      ) {
+      } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
         body.classList.remove("scroll-down");
       }
       lastScroll = currentScroll;
@@ -104,14 +82,9 @@ if (spanContainers) {
     const letters = item.children[0].textContent.split("");
     item.innerHTML = "";
     letters.forEach((el, idx) => {
-      item.innerHTML += `<span style="transition-delay: ${
-        0.07 * idx
-      }s">${el}</span>`;
+      item.innerHTML += `<span style="transition-delay: ${0.07 * idx}s">${el}</span>`;
     });
   });
 }
 
-console.log(
-  "%c Hi mom! Made with ❤️ by @dvasadva for @tirecalli",
-  "color: #ff0000; font-size: 20px;"
-);
+console.log("%c Hi mom! Made with ❤️ by @dvasadva for @tirecalli", "color: #ff0000; font-size: 20px;");
