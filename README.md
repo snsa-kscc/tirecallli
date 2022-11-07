b2616ae4d63716c90a8af455b5f610ddf7bea42f
Sinisa Koscec <snsa_kscc@asdf.local>
2022-11-07 01:15:52 +0100

    cart func

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # tirecalli
+
 Website for Tirecalligraphy fashion brand based in Zagreb, Croatia
 
 [tirecalli.com](https://tirecalli.com)
+
+# test -> production
+
+controls -> autoplay na indexu
+sandbox env -> production env - paypal
+invert na html i na img
+
+## 2DO
+
+cart page tsx react
+
+## creds
+
+sb-jetpq4948302@personal.example.com
+)omfD%9g
+
+sb-g6ih34955583@business.example.com
+G_sg9n/L
+
+https://webhook.site/#!/59f7368d-f5bf-41a0-b9be-d639d7c6b1b0
--- functions/paypal/paypal.js
+++ functions/paypal/paypal.js
@@ -1,20 +1,21 @@
+const products = require("../data/inventory.json");
 const paypal = require("@paypal/checkout-server-sdk");
 
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env;
-
 const Environment = paypal.core.SandboxEnvironment;
-
 const paypalClient = new paypal.core.PayPalHttpClient(new Environment(PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET));
 
-const storeItems = new Map([
-  [1, { price: 100, name: "Learn React Today" }],
-  [2, { price: 200, name: "Learn CSS Today" }],
-]);
+// const storeItems = new Map([
+//   [1, { price: 100, name: "Learn React Today" }],
+//   [2, { price: 200, name: "Learn CSS Today" }],
+// ]);
+
+const storeItems = new Map(products);
 
 exports.handler = async function (event, context) {
   const request = new paypal.orders.OrdersCreateRequest();
   const parsedEvent = JSON.parse(event.body);
-  const total = parsedEvent.items.reduce((sum, item) => {
+  const total = parsedEvent.reduce((sum, item) => {
     return sum + storeItems.get(item.id).price * item.quantity;
   }, 0);
   request.prefer("return=representation");
@@ -32,7 +33,7 @@ exports.handler = async function (event, context) {
             },
           },
         },
-        items: parsedEvent.items.map((item) => {
+        items: parsedEvent.map((item) => {
           const storeItem = storeItems.get(item.id);
           return {
             name: storeItem.name,
@@ -43,6 +44,30 @@ exports.handler = async function (event, context) {
             quantity: item.quantity,
           };
         }),
+        shipping: {
+          options: [
+            {
+              id: "SHIP_123",
+              label: "Shipping and Handling",
+              type: "SHIPPING",
+              selected: true,
+              amount: {
+                value: "5.00",
+                currency_code: "EUR",
+              },
+            },
+            // {
+            //   id: "SHIP_456",
+            //   label: "Pickup in Zagreb, Cro",
+            //   type: "PICKUP",
+            //   selected: false,
+            //   amount: {
+            //     value: "0.00",
+            //     currency_code: "EUR",
+            //   },
+            // },
+          ],
+        },
       },
     ],
   });
@@ -53,7 +78,10 @@ exports.handler = async function (event, context) {
       statusCode: 200,
       body: JSON.stringify({ id: order.result.id }),
     };
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ msg: err.response.body.title }),
+    };
   }
 };
