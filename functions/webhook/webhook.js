ac9e526ab515f7199e57620b25d3923fb290cd62
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-09 19:28:19 +0100

    webhook slack part4

--- functions/webhook/webhook.js
+++ functions/webhook/webhook.js
@@ -1,15 +1,13 @@
 const { SLACK_WEBHOOK } = process.env;
 
 exports.handler = async function (event, context) {
-  const desc = event.body;
-  console.log(desc);
   try {
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ text: desc }),
+      body: JSON.stringify({ text: "fgdgdgd" }),
     };
     const response = await fetch(SLACK_WEBHOOK, options);
     console.log(response);
