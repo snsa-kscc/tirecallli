2a6bde0ef3a106c9bd19c265996437cdd1fc5c97
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-10 01:08:03 +0100

    webhook part20

--- functions/webhook/webhook.js
+++ functions/webhook/webhook.js
@@ -1,23 +1,23 @@
 const { WEBHOOK_URL } = process.env;
 
 exports.handler = async function (event, context) {
-  try {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text: "jkhkhk" }),
-    };
-    const response = await fetch(WEBHOOK_URL, options);
-    return {
-      statusCode: 200,
-      body: "success",
-    };
-  } catch (err) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ msg: err }),
-    };
-  }
+  // try {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: "jkhkhk" }),
+  };
+  const response = await fetch(WEBHOOK_URL, options);
+  return {
+    statusCode: 200,
+    // body: "success",
+  };
+  // } catch (err) {
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({ msg: err }),
+  //   };
+  // }
 };
