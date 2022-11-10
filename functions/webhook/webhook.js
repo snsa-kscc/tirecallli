2f93454e6c07e5e424447919078f800d8eaabfdf
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-10 01:59:09 +0100

    webhook almost

--- functions/webhook/webhook.js
+++ functions/webhook/webhook.js
@@ -1,24 +1,24 @@
 const { WEBHOOK_URL } = process.env;
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  // try {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: "jkhkhk" }),
-  };
-  const response = await fetch(WEBHOOK_URL, options);
-  return {
-    statusCode: 200,
-    // body: "success",
-  };
-  // } catch (err) {
-  //   return {
-  //     statusCode: 400,
-  //     body: JSON.stringify({ msg: err }),
-  //   };
-  // }
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: "jkhkhk" }),
+    };
+    const response = await fetch(WEBHOOK_URL, options);
+    return {
+      statusCode: 200,
+      body: "success",
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ msg: err }),
+    };
+  }
 };
