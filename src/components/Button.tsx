f4cd26af5109f62733d0fd9cb8e2e54e8a1139e6
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-15 03:38:40 +0100

    button error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,35 @@
-import { useShoppingCart } from "../context/ShoppingCartContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ButtonProps = {
-  id?: number;
+  id: number;
   size: string;
 };
 
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
 export function Button({ id, size }: ButtonProps) {
-  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", []);
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
 
   return (
-    <button className="button button--size button--flex" onClick={() => increaseCartQuantity(222)}>
+    <button data-item={id} className="button button--size button--flex" onClick={() => increaseCartQuantity(id)}>
       <span>{size}</span>
     </button>
   );
