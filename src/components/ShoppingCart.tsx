e8f553ce41dc2444b37b06f8a65749aac3306b2f
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-20 19:16:43 +0100

    cart assets css styling

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -15,49 +15,56 @@ export function ShoppingCart() {
 
   if (cartQuantity) {
     return (
-      <>
-        <h2>Bag</h2>
-        {cartItems.map((cartItem) => {
-          const item = storeItems.find((i) => i.id === cartItem.id);
-          return (
-            <div key={cartItem.id}>
-              <img src={item.imgUrl} className="pic" alt="tirecalli-picture" />
-              <div>{item.name}</div>
-              <div>
-                Item price: {item.price}€ {cartItem.quantity > 1 && <span>x quantity {cartItem.quantity}</span>}
+      <div className="bag flex">
+        <div className="bag__content">
+          <h2>Bag</h2>
+          {cartItems.map((cartItem) => {
+            const item = storeItems.find((i) => i.id === cartItem.id);
+            return (
+              <div key={cartItem.id} className="bag__item flex">
+                <img src={item.imgUrl} className="pic" alt="tirecalli-picture" />
+                <div className="bag__description">
+                  <div>{item.name}</div>
+                  <div>
+                    Item price: {item.price}€ {cartItem.quantity > 1 && <span className="bag__item-quantity">x quantity {cartItem.quantity}</span>}
+                  </div>
+                  <div>Item total: {item.price * cartItem.quantity}€</div>
+                  <button className="btn" onClick={() => increaseCartQuantity(cartItem.id, setCartItems)}>
+                    plus
+                  </button>
+                  <button className="btn" onClick={() => decreaseCartQuantity(cartItem.id, setCartItems)}>
+                    minus
+                  </button>
+                  <button className="btn" onClick={() => removeFromCart(cartItem.id, setCartItems)}>
+                    remove
+                  </button>
+                </div>
               </div>
-              <div>Item total: {item.price * cartItem.quantity}€</div>
-              <button className="btn" onClick={() => increaseCartQuantity(cartItem.id, setCartItems)}>
-                plus
-              </button>
-              <button className="btn" onClick={() => decreaseCartQuantity(cartItem.id, setCartItems)}>
-                minus
-              </button>
-              <button className="btn" onClick={() => removeFromCart(cartItem.id, setCartItems)}>
-                remove
-              </button>
-            </div>
-          );
-        })}
-        <div>
-          Subtotal:{" "}
-          {cartItems.reduce((total, cartItem) => {
-            const item = storeItems.find((i) => i.id === cartItem.id);
-            return total + (item?.price || 0) * cartItem.quantity;
-          }, 0)}
-          €
+            );
+          })}
         </div>
-        <div>Delivery and Handling: 5€</div>
-        <div>
-          Total:{" "}
-          {cartItems.reduce((total, cartItem) => {
-            const item = storeItems.find((i) => i.id === cartItem.id);
-            return total + (item?.price || 0) * cartItem.quantity;
-          }, 5)}
-          €
+        <div className="bag__summary">
+          <h2>Summary</h2>
+          <div>
+            Subtotal:{" "}
+            {cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find((i) => i.id === cartItem.id);
+              return total + (item?.price || 0) * cartItem.quantity;
+            }, 0)}
+            €
+          </div>
+          <div>Delivery and Handling: 5€</div>
+          <div>
+            Total:{" "}
+            {cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find((i) => i.id === cartItem.id);
+              return total + (item?.price || 0) * cartItem.quantity;
+            }, 5)}
+            €
+          </div>
+          <div id="paypal-button-container"></div>
         </div>
-        <div id="paypal-button-container"></div>
-      </>
+      </div>
     );
   }
   return (
