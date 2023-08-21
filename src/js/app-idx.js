59813aa570b0991c5c7ba81a7b73287c20814f22
Sinisa Koscec <sinisa@dvasadva.com>
2023-08-21 11:37:28 +0200

    add idx text effect

--- src/js/app-idx.js
+++ src/js/app-idx.js
@@ -12,3 +12,44 @@ gsap.to(".second", {
     scrub: true,
   },
 });
+
+const textContainer = document.querySelector(".text-container");
+const words = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum quisque non tellus orci ac auctor augue. Pharetra magna ac placerat vestibulum. Risus nullam eget felis eget nunc lobortis mattis aliquam faucibus. Aliquet enim tortor at auctor urna nunc. Porttitor eget dolor morbi non arcu risus quis varius quam. Tellus cras adipiscing enim eu.
+`
+  .trim()
+  .split(" ");
+
+words.forEach((word) => {
+  const span = document.createElement("span");
+  span.textContent = word;
+  span.classList.add("mx-1", "transition", "duration-1000", "inline-block", "opacity-0");
+  textContainer.appendChild(span);
+});
+
+const spans = document.querySelectorAll(".text-container span");
+let fadedIndices = [];
+
+function fadeInRandomly() {
+  if (fadedIndices.length >= spans.length) {
+    return;
+  }
+
+  let randomIndex = -1;
+
+  while (fadedIndices.includes(randomIndex) || randomIndex === -1) {
+    randomIndex = Math.floor(Math.random() * spans.length);
+  }
+
+  spans[randomIndex].style.opacity = 1;
+  fadedIndices.push(randomIndex);
+
+  setTimeout(fadeInRandomly, 50);
+}
+
+ScrollTrigger.create({
+  trigger: textContainer,
+  start: "top center",
+  once: true,
+  onEnter: () => fadeInRandomly(),
+});
