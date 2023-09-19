d23360abdd8c76eb63c752cb636dc6966e964e50
Sinisa Koscec <sinisa@dvasadva.com>
2023-09-19 14:39:13 +0200

    add scroll animations evo and origins

--- src/js/app-heart-coeurt.js
+++ src/js/app-heart-coeurt.js
@@ -1,2 +1,27 @@
 import { gsap, ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
+
+const heartImg = document.querySelector(".heart-evo__group--second > div");
+
+gsap.to(heartImg, {
+  yPercent: -100 * heartImg.dataset.speed,
+  scrollTrigger: {
+    scrub: heartImg.dataset.speed,
+    trigger: ".heart-evo__group--second > h1",
+    start: "top 80%",
+  },
+});
+
+const originsImgs = [...document.querySelectorAll(".heart-origins img")];
+
+originsImgs.forEach((img) => {
+  gsap.from(img, {
+    xPercent: -80 * img.dataset.speed,
+    scrollTrigger: {
+      scrub: true,
+      trigger: ".heart-origins > div",
+      start: "top bottom",
+      end: "top 10%",
+    },
+  });
+});
