a1908570307cc40c8ffe0cdec672b2c63b8a0c1f
Sinisa Koscec <sinisa@dvasadva.com>
2023-10-03 02:54:55 +0200

    style displacement pic

--- src/js/app-heart-coeurt.js
+++ src/js/app-heart-coeurt.js
@@ -2,16 +2,33 @@ import { gsap, ScrollTrigger, Flip } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, Flip);
 
-gsap.fromTo(
-  ".displacement",
-  {
-    r: 0,
+gsap.to(".displacement", {
+  r: 720,
+  scrollTrigger: {
+    trigger: ".heart-evo svg",
+    scrub: true,
+    start: "top 60%",
+    end: "top 5%",
   },
-  {
-    r: 880,
-    repeat: -1,
-    duration: 10,
-    ease: "power3.inOut",
-    yoyo: true,
-  }
-);
+});
+
+gsap.to(".heart-evo__group h4:first-child", {
+  xPercent: -70,
+  scrollTrigger: {
+    trigger: ".heart-evo svg",
+    scrub: true,
+    start: "top 60%",
+    end: "top 5%",
+  },
+});
+
+gsap.to(".heart-evo__group h4:nth-of-type(2)", {
+  xPercent: 60,
+  yPercent: -60,
+  scrollTrigger: {
+    trigger: ".heart-evo svg",
+    scrub: true,
+    start: "top 60%",
+    end: "top 5%",
+  },
+});
