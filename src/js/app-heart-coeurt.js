6c3044f6a0a4102ed2bb4ebd7c60217c289f1c6f
Sinisa Koscec <sinisa@dvasadva.com>
2023-10-06 04:27:51 +0200

    style gsap pic

--- src/js/app-heart-coeurt.js
+++ src/js/app-heart-coeurt.js
@@ -32,3 +32,45 @@ gsap.to(".heart-evo__group h4:nth-of-type(2)", {
     end: "top 5%",
   },
 });
+
+const heartInspoPics = [...document.querySelectorAll(".heart-origins__inspo img")];
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".heart-origins__inspo",
+    start: "top 25%",
+    end: "+=300%",
+    scrub: true,
+    pin: true,
+  },
+});
+
+heartInspoPics.forEach((pic, idx) => {
+  pic.style.zIndex = 3 - idx;
+  tl.to(
+    pic,
+    {
+      xPercent: idx * -90,
+      opacity: 0.8 / (idx * 1.1 + 1),
+    },
+    "<"
+  );
+});
+
+gsap.fromTo(
+  "main",
+  {
+    background: "#fff",
+    color: "#000",
+  },
+  {
+    background: "#fffbdc",
+    color: "#505050",
+    scrollTrigger: {
+      trigger: ".heart-origins__copy",
+      start: "top center",
+      end: "bottom 40%",
+      scrub: true,
+    },
+  }
+);
